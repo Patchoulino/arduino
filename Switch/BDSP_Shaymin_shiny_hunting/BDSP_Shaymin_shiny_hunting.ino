7c6a8630afb18eb4a229e9849008aa6f08c1daec
--get <--get>
2022-04-08 00:09:37 -0500

    added BDSP_Shaymin_shiny_hunting/BDSP_Shaymin_shiny_hunting.ino

---
+++ Switch/BDSP_Shaymin_shiny_hunting/BDSP_Shaymin_shiny_hunting.ino
@@ -0,0 +1,55 @@
+/*
+ * BDSP Shaymin shiny hunting
+ * buttons needed
+ * HOME X A UP
+ */
+
+const int T = 300;
+int blinke = 0;
+const int HOME = 2;
+const int X = 3;
+const int A = 4;
+const int UP = 5;
+
+void setup() {
+  for (int i = 2; i <= 5; i++) { // Set all pins as outputs and 1
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
+  }
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() { // Start at battle with Shaymin so reset of the arduino will open home and restart game
+  // Restart game
+  button(HOME, T);
+  delay(500);
+  button(X, T);
+
+  for (int i = 0; i <= (40000/T/2); i++){ // 40 sec to load 
+    button(A, T);
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  button(UP, T); // Engage
+
+  for (int i = 0; i <= (11000/T/2); i++){ // animation
+    button(A, T);
+  }
+
+  blinke = 1;
+  for (int i = 0; i <= (15000/T/2); i++){ // blink and spam first move, waiting for reset
+    button(A, T);
+  }
+  blinke = 0;
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void button(int btn, int timing) {
+  digitalWrite(btn, LOW);
+  if (blinke) digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  delay(timing);
+  digitalWrite(btn, HIGH);
+  if (blinke) digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  delay(timing);
+}
