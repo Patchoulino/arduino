4eac84cffb2fd434e57e9e8e7a5466fdb786a10b
--get <--get>
2022-04-09 16:09:56 -0500

    added a new version to hunt shaymin, since it's faster to run away than to reset the game

--- Switch/BDSP_Shaymin_shiny_hunting_v2/BDSP_Shaymin_shiny_hunting_v2.ino
+++ Switch/BDSP_Shaymin_shiny_hunting_v2/BDSP_Shaymin_shiny_hunting_v2.ino
@@ -1,18 +1,20 @@
 /*
  * BDSP Shaymin shiny hunting no game reset
  * buttons needed
- * HOME X A UP
+ * A UP DOWN B
  */
 
 const int T = 300;
 int blinke = 0;
-const int HOME = 2;
-const int X = 3;
+//const int HOME = 2;
+//const int X = 3;
 const int A = 4;
 const int UP = 5;
+const int DOWN = 6;
+const int B = 7;
 
 void setup() {
-  for (int i = 2; i <= 5; i++) { // Set all pins as outputs and 1
+  for (int i = 2; i <= 7; i++) { // Set all pins as outputs and 1
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
   }
@@ -20,29 +22,35 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() { // Start at battle with Shaymin so reset of the arduino will open home and restart game
-  // Restart game
-  button(HOME, T);
-  delay(500);
-  button(X, T);
-
-  for (int i = 0; i <= (40000/T/2); i++){ // 40 sec to load 
-    button(A, T);
-  }
+void loop() { // Start in front of spawn
+  int running_time = 3000;  // Run outside to reset encounter
+  digitalWrite(DOWN, LOW);
+  digitalWrite(B, LOW);
+  delay(running_time);
+  digitalWrite(DOWN, HIGH);
+  digitalWrite(UP, LOW);  // Run back
+  delay(running_time-100);
+  digitalWrite(UP, HIGH);
+  digitalWrite(B, HIGH);  
 
   digitalWrite(LED_BUILTIN, HIGH);
-  button(UP, T); // Engage
+  button(A, T); // Engage
 
-  for (int i = 0; i <= (11000/T/2); i++){ // animation
+  for (int i = 0; i <= (10000/T/2); i++){ // animation
     button(A, T);
   }
 
   blinke = 1;
-  for (int i = 0; i <= (15000/T/2); i++){ // blink and spam first move, waiting for reset
-    button(A, T);
+  for (int i = 0; i <= (12500/T/2); i++){ // blink and spam B
+    button(B, T);
   }
   blinke = 0;
   digitalWrite(LED_BUILTIN, LOW);
+
+  button(UP, T); // Run away
+  button(A, T);
+  delay(3800);
+  button(B, T); // Clear Shaymin message
 }
 
 void button(int btn, int timing) {
