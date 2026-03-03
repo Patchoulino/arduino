7394499f7161346322bfb57775e67076d7549634
Abraham Aguilar <abagl@outlook.com>
2026-03-03 10:12:47 -0600

    updating repository

---
+++ pokemon/FRLG_FrameTimerV2/FRLG_FrameTimerV2.ino
@@ -0,0 +1,426 @@
+/*
+ * FRLG Arduino assisted FlowTimer Switch edition
+ */
+#include <Streaming.h>
+
+// Arduino variables
+String inString1 = "";  // string to hold input
+String inString2 = "";  // string to hold input
+boolean shift = 0;
+int last_char = 0;
+int new_char = 0;
+boolean serial_monitor = 0;
+// Timing variables
+const byte T = 100;
+double timer_frame_ms_math = 0;
+unsigned long timer_frame_ms = 0;
+unsigned long timer_frame = 0;
+unsigned long timer_seed = 0;
+unsigned long offset_btn = 0;
+int offset = 0;
+// Game variables
+int pkmn_select = 1;
+// Button variables
+const byte HOME = 2;
+const byte UP = 3;
+const byte RIGHT = 4;
+const byte DOWN = 5;
+const byte LEFT = 6;
+const byte B = 7;
+const byte A = 8;
+const byte START = 9;
+const byte X = 9;
+const byte SELECT = 10;
+const byte Y = 10;
+const byte R = 11;
+const byte L = 12;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  for (int i = 2; i <= 13; i++)  pinMode(i, OUTPUT);
+  for (int i = 2; i <= 13; i++)  digitalWrite(i, HIGH);
+  //digitalWrite(POWER, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+  // Read serial input:
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    if (isDigit(inChar)) {
+      // convert the incoming byte to a char and add it to the string:
+      if ( shift == 0 ) inString1 += (char)inChar;
+      else              inString2 += (char)inChar;
+    }
+    else  
+    {
+      switch (inChar) {
+        case '/':
+          shift++;
+          break;
+        case 'Z': // Demo
+          while (true) {
+            for (int i = 3; i <= 12; i++){
+              digitalWrite(i, 0);
+              delay(100);
+              digitalWrite(i, 1);
+              delay(100);
+            }
+          }
+          break;
+        case 'R':
+          restart_game_fast();
+          break;
+        case 'z':
+          button(HOME, T, T);
+          break;
+        case 'f':
+          button(A, T, T);
+          break;
+        case 'r':
+          button(B, T, T);
+          break;
+        case 'w':
+          button(UP, T, T);
+          break;
+        case 's':
+          button(DOWN, T, T);
+          break;
+        case 'a':
+          button(LEFT, T, T);
+          break;
+        case 'd':
+          button(RIGHT, T, T);
+          break;
+        case 'q':
+          button(L, T, T);
+          break;
+        case 'e':
+          button(R, T, T);
+          break;
+        case 'c':
+          button(START, T, T);
+          break;
+        case 'x':
+          button(SELECT, T, T);
+          break;
+        case '\n':
+          //serial_monitor = 1;
+          if (isDigit(last_char)) timer_update();
+          break;
+        case 'S': // Started via soft reset
+          starter_sh();
+          break;
+        case '!':
+          frlg_rng_starter();
+          break;
+        case '@':
+          while (Serial.available() > 0)  inChar = Serial.read();
+          pkmn_select = serial_input();
+          frlg_gamecorner(pkmn_select);  // 1 Abra | 2 Clef | 3 Dratini | 4 Scy | 5 Pory
+          break;
+        case '#':
+          frlg_sweet_scent();
+          break;
+        case '$':
+          frlg_legendary();
+          break;
+        case '%':
+          frlg_magikarp();
+          break;
+        case '*':
+          for (int i = 1; i <= 2100; i++)  button(A, T, T); // 7 min of A spam
+          break;
+        case ',':
+          frlg_new_save();
+          name_patch();
+          frlg_pid_sid();
+          break;
+        case '<':
+          frlg_new_save();
+          name_pau();
+          frlg_pid_sid();
+          break;
+      }
+    }
+    last_char = inChar;
+  }
+}
+
+void starter_sh() {
+  while (true){
+    restart_game_fast();
+    for (int i = 1; i <= 35; i++)  button(A, T, T); // 7 Sec  // Start game
+    for (int i = 1; i <= 15; i++)  button(B, T, T); // 3 Sec  // Skip Recap
+    for (int i = 1; i <= 16; i++)  button(A, T, T); // 3 Sec  // Select Starter
+    for (int i = 1; i <= 60; i++)  button(B, T, T); // 12 Sec // Spam B while rival grabs starter
+    button(START, T, T);  // check pokemon
+    delay(300);
+    for (int i = 1; i <= 10; i++)  button(A, T, T); // 2 Sec
+    delay(2000);
+    button(RIGHT, T, T);
+    delay(5000);
+  }
+}
+
+void clear_variables() { 
+  inString1 = "";
+  inString2 = "";
+  shift = 0;
+}
+
+void timer_update() {
+  timer_frame = inString1.toInt();
+  timer_seed = inString2.toInt();
+  Serial << "FRLG timer: " << timer_frame << "/" << timer_seed << endl;
+  clear_variables();
+}
+
+void restart_game_fast() {
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(X, LOW);
+  digitalWrite(Y, LOW);
+  delay(T);
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(X, HIGH);
+  digitalWrite(Y, HIGH);
+  delay(T);
+}
+
+void restart_game() {
+  button(HOME, T, T);
+  delay(500);
+  button(X, T, T);
+  for (int i = 1; i <= 15; i++)  button(A, T, T); // 3 Sec
+}
+
+void poweron_sequence() {
+  restart_game();
+  offset = 4110;  // No GBA boot logo
+  unsigned long timer_seed_fix = timer_seed - offset;
+  //Serial << "timer_seed: " << timer_seed << " timer_seed_fix:" << timer_seed_fix << endl;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(timer_seed_fix);
+  digitalWrite(LED_BUILTIN, LOW);
+  button(A, 3500, 200);
+  button(A, T, T);
+  for (int i = 1; i <= 17; i++)  button(B, T, T); // 4 Sec
+}
+
+void frlg_gamecorner(int select) {  // 1 Abra | 2 Clef | 3 Dratini | 4 Scy | 5 Pory
+  int timer_offset = 111; //FR_Og
+  timer_frame_ms_math = ((timer_frame + timer_offset) * 1000) / 60;
+  timer_frame_ms = lround(timer_frame_ms_math); 
+  Serial << "FRLG gamecorner: " << timer_frame_ms << "/" << timer_seed << " | Selected: " << pkmn_select << endl;
+  poweron_sequence();
+  for (int i = 1; i <= 7; i++)  button(A, T, T); 
+  for (int i = 1; i <= select; i++)  button(DOWN, T, T);
+  for (int i = 1; i <= 2; i++)  button(A, T, T);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(timer_frame_ms);
+  digitalWrite(LED_BUILTIN, LOW);
+  button(A, T, T);
+  check_pkmn();
+}
+
+void frlg_sweet_scent() {
+  int timer_offset = -231;  // FR_Og WIP
+  timer_frame_ms_math = ((timer_frame + timer_offset) * 1000) / 60;
+  timer_frame_ms = lround(timer_frame_ms_math); 
+  Serial << "FRLG sweetscent: " << timer_frame_ms << "/" << timer_seed << endl;
+  poweron_sequence();
+  delay(100);
+  button(START, T, T);
+  button(DOWN, T, T);
+  button(A, T, T);
+  delay(1000);
+  button(DOWN, T, T);
+  button(A, T, T);
+  button(DOWN, T, T);
+  delay(timer_frame_ms - offset_btn);
+  button(A, T, T);
+}
+
+int serial_input() {
+  Serial << "FRLG selected: ";
+  while (Serial.available() == 0) delay(100);
+  int input_char2 = Serial.read() - '0';
+  Serial << input_char2 << endl;
+  return input_char2;
+}
+
+void check_pkmn() {
+  for (int i = 1; i <= 20; i++)  button(B, T, T); // 4 Sec
+  button(START, T, T);
+  delay(100);
+  button(DOWN, T, T);
+  delay(300);
+  button(A, T, T);
+  delay(1500);
+  button(UP, T, T);
+  button(UP, T, T);
+  for (int i = 1; i <= 3; i++)  button(A, T, T); // 1 Sec
+  delay(5000);
+  button(RIGHT, T, T);
+}
+
+void frlg_legendary() { // $
+  timer_frame_ms_math = ((timer_frame - 497) * 1000.0) / 120.0;
+  timer_frame_ms = timer_frame_ms_math + 4; 
+  Serial << "FRLG static_legendary: " << timer_frame_ms << "/" << timer_seed << endl;
+  poweron_sequence();
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(timer_frame_ms);
+  digitalWrite(LED_BUILTIN, LOW);
+  button(A, T, T);
+  check_pkmn();
+}
+
+void frlg_magikarp() {  // %
+  int timer_offset = 0;
+  offset_btn = 3000; // Extra buttons delay in ms
+  timer_frame_ms_math = ((timer_frame + timer_offset) * 1000.0) / 120.0;
+  timer_frame_ms = timer_frame_ms_math - offset_btn; 
+  Serial << "FRLG magikarp: " << timer_frame_ms << "/" << timer_seed << endl;
+  poweron_sequence();
+  for (int i = 1; i <= 15; i++)  button(A, T, T); 
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(timer_frame_ms);
+  digitalWrite(LED_BUILTIN, LOW);
+  button(A, T, T);
+  check_pkmn();
+}
+
+void frlg_rng_starter() {
+  //int timer_offset = 102; //LG
+  //timer_frame_ms_math = ((timer_frame + timer_offset) * 1000.0) / 60.0;
+  timer_frame_ms_math = (timer_frame * 1000.0) / 120.0;
+  timer_frame_ms = timer_frame_ms_math; 
+  Serial << "FRLG starter: " << timer_frame_ms << "/" << timer_seed << endl;
+  poweron_sequence();
+  for (int i = 1; i <= 15; i++)  button(A, T, T); // 3 Sec
+  delay(timer_frame_ms);
+  button(A, T, T);
+  for (int i = 1; i <= 65; i++)  button(B, T, T); // 13 Sec
+  button(START, T, T);
+  delay(300);
+  for (int i = 1; i <= 10; i++)  button(A, T, T); // 2 Sec
+  delay(2000);
+  button(RIGHT, T, T);
+}
+
+void name_puto() {
+  for (int i = 1; i <= 2; i++)  button(DOWN, T, T);
+  for (int i = 1; i <= 3; i++)  button(RIGHT, T, T);
+  button(A, T, T);  // P
+  for (int i = 1; i <= 2; i++)  button(LEFT, T, T);
+  button(DOWN, T, T);
+  button(A, T, T);  // u
+  button(LEFT, T, T);
+  button(A, T, T);  // t
+  button(SELECT, T, T); // lowercase
+  delay(500);
+  button(SELECT, T, T); // Numbers
+  delay(500);
+  for (int i = 1; i <= 3; i++)  button(UP, T, T);
+  button(A, T, T);  // 0
+  button(START, T, T);
+}
+
+void name_patch() {
+  for (int i = 1; i <= 2; i++)  button(DOWN, T, T);
+  for (int i = 1; i <= 3; i++)  button(RIGHT, T, T);
+  button(A, T, T);  // P
+  button(SELECT, T, T); // lowercase
+  delay(500);
+  for (int i = 1; i <= 3; i++)  button(LEFT, T, T);
+  for (int i = 1; i <= 2; i++)  button(UP, T, T);
+  button(A, T, T);  // a
+  for (int i = 1; i <= 3; i++)  button(DOWN, T, T);
+  button(A, T, T);  // t
+  for (int i = 1; i <= 3; i++)  button(UP, T, T);
+  for (int i = 1; i <= 2; i++)  button(RIGHT, T, T);
+  button(A, T, T);  // c 
+  button(DOWN, T, T);
+  button(LEFT, T, T);
+  button(A, T, T);  // h
+  button(START, T, T);
+}
+
+void name_pau() {
+  for (int i = 1; i <= 2; i++)  button(DOWN, T, T);
+  for (int i = 1; i <= 3; i++)  button(RIGHT, T, T);
+  button(A, T, T);  // P
+  button(SELECT, T, T); // lowercase
+  delay(500);
+  for (int i = 1; i <= 3; i++)  button(LEFT, T, T);
+  for (int i = 1; i <= 2; i++)  button(UP, T, T);
+  button(A, T, T);  // a
+  for (int i = 1; i <= 3; i++)  button(DOWN, T, T);
+  button(RIGHT, T, T);
+  button(A, T, T);  // u
+  button(START, T, T);
+}
+
+void frlg_pid_sid() {
+  /*
+  * Tools > Researcher https://www.youtube.com/watch?v=xXz2GCSy6HA
+  * TID DEC to HEX, use as seed
+  * Custom1 16Bit High / 1
+  * Initial Advances 1147, or just 1149
+  * Max Advances 5
+  * Generate
+  */
+  int timer_offset = -243;  // don't remember
+  offset_btn = 6200; // Extra buttons delay in ms from rival name + delay before
+  timer_frame_ms_math = ((1149.0 + timer_offset) * 1000.0) / 60.0;
+  timer_frame_ms = timer_frame_ms_math - offset_btn; 
+  Serial << "timer_frame_ms: " << timer_frame_ms << " | offset_btn: " << offset_btn << endl;
+  //timer_frame_ms = 15100;
+  //timer_frame_ms = 15101;  //1149!! or 13.14273281 ms per frame
+
+  for (int i = 1; i <= 33; i++) button(A, T, T); // 6.6 Sec
+  delay(100);
+  button(A, T, T);  // Rival name
+  delay(900);
+  name_puto();
+  button(A, T, T);
+  delay(300);
+
+  for (int i = 1; i <= 24; i++) button(A, T, T); // 4.8 Sec
+  delay(timer_frame_ms);
+  button(A, T, T);
+  digitalWrite(LED_BUILTIN, HIGH);
+  for (int i = 1; i <= 35; i++) button(B, T, T);
+  button(START, T, T);
+  button(DOWN, T, T);
+  button(A, T, T);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void frlg_new_save() {
+  restart_game();
+  for (int i = 1; i <= 43; i++)  button(A, T, T); // 8.4 Sec
+  delay(400);
+  button(DOWN, T, T);
+  delay(400);
+  for (int i = 1; i <= 113; i++) button(A, T, T); // 22.6 Sec
+  delay(200);
+  button(DOWN, T, T); // GIRL
+  for (int i = 1; i <= 15; i++)  button(A, T, T); // 3 Sec
+}
+
+void button(int btn, int timing_on, int timing_off) {
+  if (serial_monitor) Serial << "Button [" << btn << "]: on " << timing_on << endl;
+  digitalWrite(btn, LOW);
+  delay(timing_on);
+  if (serial_monitor) Serial << "Button [" << btn << "]: off " << timing_off << endl;
+  digitalWrite(btn, HIGH);
+  delay(timing_off);
+  offset_btn = offset_btn + timing_on + timing_off;
+}
---
+++ pokemon/FRLG_Master_script/FRLG_Master_script.ino
@@ -0,0 +1,529 @@
+/*
+PKMN Master Script ZA edition
+List of functions:
+speedrun (A spam)
+run_backnforth
+fasttravel
+autobattle
+zoneX
+fossils
+*/
+
+const bool groov = true;
+const int switcho = 2;
+const int gate_time = 2000;
+const int backnforth_time = 3500;
+
+// 8 leds shield
+//const int OUT[] = {3, 4, 5, 6, 9, 10, 11, 12};
+//const int OUT_LENGTH = 7;
+//const int pause_time = 35;
+// 4 leds shield
+const int OUT[] = {3, 5, 6, 12};
+const int OUT_LENGTH = 3;
+const int pause_time = 70;
+  
+const int IN[] = {A0, A1, A2, A3, A4, A5, 8, 7, 2};
+const int IN_LENGTH = 8;
+
+const int Y = 0;
+const int B = 1;
+const int A = 2;
+const int X = 3;
+const int L = 4;
+const int R = 5;
+const int ZL = 6;
+const int ZR = 7;
+const int MINUS = 8;
+const int PLUS = 9;
+const int SELECT = 8;
+const int START = 9;
+const int LSTICK = 10;
+const int RSTICK = 11;
+const int HOME = 12;
+const int CAPTURE = 13;
+const int UP = 0;
+const int RIGHT = 90;
+const int DOWN = 180;
+const int LEFT = 270;
+const int RELEASE = -1;
+
+//https://github.com/HackerLoop/Arduino-JoyCon-Library-for-Nintendo-Switch/blob/master/examples/SwitchJoystickTest/SwitchJoystickTest.ino
+//C:\Users\USER\AppData\Local\Arduino15\packages\arduino\hardware\avr\current_version\boards.txt
+#include "SwitchJoystick.h"
+
+// Create Joystick
+SwitchJoystick_ Joystick;
+
+// Set to true to test "Auto Send" mode or false to test "Manual Send" mode.
+const bool testAutoSendMode = true;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  for(int i = 0; i <= IN_LENGTH; i++)   pinMode(IN[i], INPUT);
+  for(int i = 0; i <= OUT_LENGTH; i++)  pinMode(OUT[i], OUTPUT);
+  groovy();
+  
+  if (testAutoSendMode) Joystick.begin();
+  else  Joystick.begin(false);
+
+  reset_joysticks();
+  Joystick.sendState();
+  for (int i = 0; i <= 2; i++)  button(RSTICK, 250);
+}
+
+void loop() {
+  int x = bin2byte();
+  int T = 100;
+  int direction = 0;
+  int speed = 0;
+  switch (x) {  // Lower vaue is the one closest to USB
+    case 0:    // 0000 0000
+      speedrun(T, 2); // benches
+      break;
+    case 1:    // 0000 0001
+    case 2:    // 0000 0010
+    case 3:    // 0000 0011
+    case 4:    // 0000 0100
+    case 5:    // 0000 0101
+    case 6:    // 0000 0110
+    case 7:    // 0000 0111
+    case 8:    // 0000 1000
+    case 9:    // 0000 1001
+      fasttravel(T, switcho, x); // fast travel
+      break;
+    case 16:    // 0001 0000
+      direction = 2;
+      fasttravel(T, 4, direction); // fast travel same location
+      break;
+    case 17:    // 0001 0001
+    case 18:    // 0001 0010
+    case 19:    // 0001 0011
+    case 20:    // 0001 0100
+    case 21:    // 0001 0101
+    case 22:    // 0001 0110
+    case 23:    // 0001 0111
+    case 24:    // 0001 1000
+    case 25:    // 0001 1001
+      direction = (x - 16);
+      speed = switcho + 1;
+      fasttravel(T, speed, direction); // small window or cafeterias
+      break;
+    case 49:    // 0011 0001
+    case 50:    // 0011 0010
+    case 51:    // 0011 0011
+    case 52:    // 0011 0100
+    case 53:    // 0011 0101
+    case 54:    // 0011 0110
+    case 55:    // 0011 0111
+    case 56:    // 0011 1000
+    case 57:    // 0011 1001
+      direction = (x - 48);
+      speed = switcho + 2;
+      fasttravel(T, speed, direction); // smaller window or cafeterias
+      break;
+    case 64:    // 0100 0000
+      direction = 8;
+      run_backnforth(T, backnforth_time, direction);
+      break;
+    case 65:    // 0100 0001 (1)
+    case 66:    // 0100 0010 (2)
+    case 67:    // 0100 0011 (3)
+    case 68:    // 0100 0100 (4)
+    case 69:    // 0100 0101 (5)
+    case 70:    // 0100 0110 (6)
+    case 71:    // 0100 0111 (7)
+    case 72:    // 0100 1000 (8)
+    case 73:    // 0100 1001 (9)
+      direction = (x - 64);
+      run_backnforth(T, backnforth_time, direction);
+      break;
+    case 128:    // 1000 0000 (0)
+      speedrun(T, 5); // A spam + direction
+      break;
+    case 129:    // 1000 0001 (1)
+    case 130:    // 1000 0010 (2)
+    case 131:    // 1000 0011 (3)
+    case 132:    // 1000 0100 (4)
+    case 133:    // 1000 0101 (5)
+    case 134:    // 1000 0110 (6)
+    case 135:    // 1000 0111 (7)
+    case 136:    // 1000 1000 (8)
+    case 137:    // 1000 1001 (9)
+      direction = (x - 128);
+      speedrun(T, direction); // A spam + direction
+      break;
+    case 192:    // 1100 0000
+      autobattle(50);
+      break;
+    case 193: // 1100 0001  Zone 01
+    case 194: // 1100 0010  Zone 02
+    case 195: // 1100 0011  Zone 03
+    case 196: // 1100 0100  Zone 04
+    case 197: // 1100 0101  Zone 05
+    case 198: // 1100 0110  Zone 06
+    case 199: // 1100 0111  Zone 07
+    case 200: // 1100 1000  Zone 08
+    case 201: // 1100 1001  Zone 09
+    case 202: // 1100 1010  Zone 10
+    case 203: // 1100 1011  Zone 11
+    case 204: // 1100 1100  Zone 12
+    case 205: // 1100 1101  Zone 13
+    case 206: // 1100 1110  Zone 14
+    case 207: // 1100 1111  Zone 15
+    case 208: // 1101 0000  Zone 16
+    case 209: // 1101 0001  Zone 17
+    case 210: // 1101 0010  Zone 18
+    case 211: // 1101 0011  Zone 19
+    case 212: // 1101 0100  Zone 20 water bench
+    case 213: // 1101 0101  Zone 20 grass bench
+    case 214: // 1101 0110  Zone 20 fire starters
+    case 215: // 1101 0111  Sewers litw
+    case 216: // 1101 1000  Lab 30f
+    case 217: // 1101 1001  Lab 60f
+    case 218: // 1101 1010  Lab Switch 1 30f
+    case 219: // 1101 1011  Lab Switch 1 60f
+    case 220: // 1101 1100  Lab Switch 1 120f
+      direction = (x - 192);
+      zone(T, direction);
+      break;
+  }
+}
+
+int bin2byte(){
+  int c = 0;
+  for(int i = 0; i < IN_LENGTH; i++)
+  {
+    if(!digitalRead(IN[i]))  
+    {
+      double result = pow(2, i);
+      int result_int = (int)(result + 0.5);
+      c = c + result_int;
+    }
+  }
+  return c;
+}
+
+void groovy() {
+  if (groov) {
+    for(int i = 0; i <= OUT_LENGTH; i++) {
+      digitalWrite(OUT[i], HIGH);
+      delay(pause_time);
+      digitalWrite(OUT[i], LOW);
+    }
+  }
+  else {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(250);
+  }
+}
+
+void paused(){
+  reset_joysticks();
+  while(!digitalRead(IN[IN_LENGTH]))  groovy();
+  while(digitalRead(IN[IN_LENGTH]))   groovy();
+  while(!digitalRead(IN[IN_LENGTH]))  groovy();
+}
+
+void wait_forever(){
+  reset_joysticks();
+  while(true){
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    delay(500);
+  }
+}
+
+void wait(int total_time){
+  unsigned int interval = 10;
+  for (unsigned int i = 0; i < total_time; i += interval) {
+    if(!digitalRead(IN[IN_LENGTH]))  paused();
+    delay(interval);
+  }
+}
+
+void reset_joysticks(){
+  Joystick.setXAxis(128);
+  Joystick.setYAxis(128);
+  Joystick.setZAxis(128);
+  Joystick.setRzAxis(128);
+}
+
+void led_progress(int cycle, int range){
+  float leds = ((cycle * (OUT_LENGTH + 1)) / range);  // previously 8.0
+  for (int i = 0; i <= OUT_LENGTH; i++) {
+    if (leds >= (i + 1))  digitalWrite(OUT[i], HIGH);
+    else                  digitalWrite(OUT[i], LOW);
+  }
+}
+
+void button(int btn, int timing) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  Joystick.pressButton(btn);
+  delay(timing);
+  Joystick.releaseButton(btn);
+  delay(timing);
+}
+
+void button_hold(int btn, int timing) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  Joystick.pressButton(btn);
+  delay(timing);
+}
+
+void button_release(int btn, int timing) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  Joystick.releaseButton(btn);
+  delay(timing);
+}
+
+void dpad(int btn, int timing) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  Joystick.setHatSwitch(btn);
+  delay(timing);
+  Joystick.setHatSwitch(RELEASE);
+  delay(timing);
+}
+
+void lstick(int direction, int timing) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  int X = (int)((((direction - 1) % 3) * 127.5) + 0.5);
+  int Y = (int)(255 - (((direction - 1) / 3) * 127.5 + 0.5));
+  Joystick.setXAxis(X);
+  Joystick.setYAxis(Y);
+  wait(timing);
+  Joystick.setXAxis(128);
+  Joystick.setYAxis(128);
+}
+
+void lstick_fixed(int direction) {
+  if(!digitalRead(IN[IN_LENGTH]))  paused();
+  int X = (int)((((direction - 1) % 3) * 127.5) + 0.5);
+  int Y = (int)(255 - (((direction - 1) / 3) * 127.5 + 0.5));
+  Joystick.setXAxis(X);
+  Joystick.setYAxis(Y);
+}
+
+void speedrun(int T, int direction) {
+  lstick_fixed(direction);
+  button(A, T);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void fasttravel_confirmation(int T){
+  int time = 0;
+  wait(300);
+  for (int i = 0; i <= (1800/T/2); i++)  button(A, T);  // 2 sec
+  time = 10800 + (-4200 * switcho);  // loading screen time for each switcho
+  wait(time);
+}
+
+void fasttravel(int T, int timing, int direction) {
+  int time = 0;
+  button(START, T);
+  time = 220 + (-50 * timing);      // variable time left stick
+  lstick(direction, time);
+  fasttravel_confirmation(T);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void autobattle(int T) {
+  button_hold(ZL, T);
+  for (int i = 0; i < 3; i++)  button(A, T);
+  button(X, T);
+  button_release(ZL, T);
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
+
+void run_line(int T, int duration, int direction) {
+    lstick_fixed(direction);
+    button(B, T);
+    wait(duration);
+    lstick(5, T);
+}
+
+void walk_line(int T, int duration, int direction) {
+    lstick_fixed(direction);
+    wait(duration);
+    lstick(5, T);
+}
+
+void run_backnforth(int T, int duration, int direction) {
+  for (int i = 1; i <= 2; i++) {
+    run_line(T, duration, direction);
+    wait(100);
+    direction = 10 - direction;
+    duration = duration + 7; // 10 too down, 5 too front
+  }
+}
+
+void enter_zone(int T){ // walk forward and press A
+  run_line(T, 500, 8);
+  button(A, T);
+  wait(gate_time);
+}
+
+void soft_reset(int T) {
+  button(HOME, T);
+  wait(800);
+  button(X, T);
+  wait(400);
+  for (int i = 0; i <= (12000/T/2); i++)  button(A, T);  // Exit and Start game
+}
+
+void fossils(int T, int cycles) {
+  soft_reset(T);
+  for (int i = 1; i < cycles; i++){  // 30 fossils
+    if (groov) led_progress(i, cycles);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    for (int i = 0; i <= (15350/50/2); i++)  button(A, 50); // 15600
+  }
+  for (int i = 0; i <= (6000/T/2); i++)  button(B, T);
+  button(X, T);
+  wait(400);
+  button(A, T);
+  wait(2000);
+  dpad(UP, T);
+  paused();
+  for (int i = 0; i <= 2; i++)  button(RSTICK, 250);  // in case the switch fell asleep
+}
+
+void zone(int T, int zone){
+  int time = 0;
+    switch (zone) {
+    case 1:   // 1100 0001
+      break;
+    case 2:   // 1100 0010
+      break;
+    case 3:   // 1100 0011
+      break;
+    case 4:   // 1100 0100
+      break;
+    case 5:   // 1100 0101
+      break;
+    case 6:   // 1100 0110  bunearys
+      run_line(T, 500, 4);
+      button(START, T);
+      lstick(8, 100);
+      fasttravel_confirmation(T);
+      break;
+    case 7:   // 1100 0111
+      break;
+    case 8:   // 1100 1000
+      break;
+    case 9:   // 1100 1001
+      break;
+    case 10:  // 1100 1010
+      break;
+    case 11:  // 1100 1011
+      break;
+    case 12:  // 1100 1100
+      break;
+    case 13:  // 1100 1101  scyther
+      enter_zone(T);
+      run_line(T, 1500, 7);
+      button(START, T);
+      fasttravel_confirmation(T);
+      break;
+    case 14:  // 1100 1110  heliolisk
+      enter_zone(T);
+      run_backnforth(T, 1500, 8);
+      button(A, T);
+      wait(gate_time);
+      button(START, T);
+      lstick(2, 50);
+      fasttravel_confirmation(T);
+      break;
+    case 15:  // 1100 1111
+      break;
+    case 16:  // 1101 0000  drampa soft resets
+      soft_reset(T);
+      wait(3000); // drampa spawn
+      for (int i = 1; i <= 18; i++){ // 280ms every groove = 8 * 35ms || 18 = 5040ms
+        groovy();
+      }
+      break;
+    case 17:  // 1101 0001  drampa walks
+      for (int i = 1; i <= 25; i++){ 
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        run_line(T, 500, 7);
+        run_line(T, 2000, 4);
+        run_line(T, 1000, 7);
+        button(START, T);
+        lstick(4, 50);
+        fasttravel_confirmation(T);
+      }
+      button(HOME, T);
+      paused();
+      break;
+    case 18:  // 1101 0010  clefairy walks
+      for (int i = 1; i <= 60; i++){ 
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+        run_line(T, 500, 7);
+        run_line(T, 2000, 4);
+        run_line(T, 1000, 7);
+        button(START, T);
+        lstick(4, 50);
+        fasttravel_confirmation(T);
+      }
+      button(HOME, T);
+      paused();
+      break;
+    case 19:  // 1101 0011  Zone 19 - does not work with clefa/drampa
+      run_line(T, 500, 7);
+      run_line(T, 2000, 4);
+      run_line(T, 1000, 7);
+      button(START, T);
+      lstick(4, 50);
+      fasttravel_confirmation(T);
+      break;
+    case 20:  // 1101 0100  Zone 20 tower - 6 alphas bench next to water
+      run_backnforth(T, 1500, 8);
+      lstick_fixed(2);
+      for (int i = 0; i <= (3000/T/2); i++)  button(A, T);  // 3 sec # bench
+      reset_joysticks();
+      wait(14800);
+      break;
+    case 21:  // 1101 0101  Zone 20 tower - 6 alphas bench next to grass
+      run_line(T, 500, 6);
+      button(L, T);
+      run_line(T, 400, 2);
+      run_line(T, 10, 6);
+      for (int i = 0; i <= (3000/T/2); i++)  button(A, T);  // 3 sec # bench
+      reset_joysticks();
+      wait(14800);
+      break;
+    case 22:  // 1101 0110  Zone 20 - fire starters
+      run_line(T, 3000, 7);
+      run_line(T, 1000, 4);
+      run_line(T, 2000, 7);
+      button(START, T);
+      wait(200);
+      button(Y, T);
+      fasttravel_confirmation(T);
+      break;
+    case 23:  // 1101 0111  sewers 1 litwick
+      run_line(T, 4000, 8);
+      wait(100);
+      run_line(T, 4100, 2);
+      wait(100);
+      break;
+    case 24:  // 1101 1000  Lab 30f
+      fossils(T, 30);
+      break;
+    case 25:  // 1101 1001  Lab 60f
+      fossils(T, 60);
+      break;
+    case 26:  // 1101 1010  Lab Switch1 30f
+      fossils(T, 35);
+      break;
+    case 27:  // 1101 1011  Lab Switch2 60f
+      fossils(T, 70);
+      break;
+    case 28:  // 1101 1011  Lab Switch2 120f
+      fossils(T, 140);
+      break;
+  }
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+}
